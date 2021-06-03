ab1c04fe0b190ee07d681f6c14ad5abc7fba8437
Rafael Pirotto <pirotto@outlook.com>
2021-06-03 17:56:35 -0300

    Removed unnecessary Container

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 
 import PopularMoviesReducer from './Containers/PopularContainer/duck/reducers';
-import MovieReducer from './Containers/Movie/duck/reducers';
 import MyListReducer from './Containers/MyListContainer/duck/reducers';
 import ContinueWatchingReducer from './Containers/ContinueWatchingContainer/duck/reducers';
 
 const appReducer = combineReducers({
   popularMovies: PopularMoviesReducer,
-  movie: MovieReducer,
   myListMovies: MyListReducer,
   continueWatchingMovies: ContinueWatchingReducer,
 });
