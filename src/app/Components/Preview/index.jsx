8bfb632e33e24d85a654eec6ce13b6b865b08843
Rafael Pirotto <pirotto@outlook.com>
2021-06-08 13:01:23 -0300

    Volume button added. Watch later icon fixed

--- src/app/Components/Preview/index.jsx
+++ src/app/Components/Preview/index.jsx
@@ -4,6 +4,8 @@ import backgroundImage from './assets/home-background.png';
 import clapperboard from './assets/clapperboard.png';
 import imdbLogo from './assets/imdb-logo.png';
 import watchTrailer from './assets/watch-trailer.png';
+import volume from './assets/volume.png';
+import watchLater from './assets/watch-later.svg';
 import styles from './styles/Preview.module.css';
 
 import { StarRating } from '../../../common/Components';
@@ -32,7 +34,7 @@ const Preview = ({ movie }) => {
           <div className="text-9xl font-bold">BLACK MIRROR</div>
           <div className="flex flex-grow justify-end items-center min-w-max">
             <button
-              className={`font-semibold rounded-3xl p-6 flex items-center h-5 ${styles.buttonBorder}`}
+              className={`font-semibold rounded-3xl p-6 flex items-center h-5 ${styles.buttonBorder} ${styles.withoutOutline}`}
             >
               PG 13
             </button>
@@ -61,18 +63,21 @@ const Preview = ({ movie }) => {
         <div className="flex mt-40" style={{ color: '#92AAD7' }}>
           <div className="flex items-center">
             <div>
-              <img src={watchTrailer} alt="watch-later" />
+              <img src={watchLater} alt="watch-later" className={styles.clickable} />
             </div>
             <div className="ml-3">Watch Later</div>
           </div>
           <div className="flex items-center ml-12">
             <div>
-              <img src={watchTrailer} alt="watch-trailer" />
+              <img src={watchTrailer} alt="watch-trailer" className={styles.clickable} />
             </div>
             <div className="ml-3">Watch trailer</div>
           </div>
           <div className="ml-12">
-            <img src={imdbLogo} alt="imdb-logo" />
+            <img src={imdbLogo} alt="imdb-logo" className={styles.clickable} />
+          </div>
+          <div className="flex justify-end flex-grow">
+            <img src={volume} alt="volume" className={styles.clickable} />
           </div>
         </div>
       </div>
