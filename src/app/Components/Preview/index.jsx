a62b857def20fd0aa59d528128b76a58f6c5d500
Rafael Pirotto <pirotto@outlook.com>
2021-06-08 11:40:28 -0300

    Clapperboard and stars added to preview

--- src/app/Components/Preview/index.jsx
+++ src/app/Components/Preview/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import backgroundImage from './assets/home-background.png';
+import clapperboard from './assets/clapperboard.png';
 import styles from './styles/Preview.module.css';
+
+import { StarRating } from '../../../common/Components';
 
 const Preview = ({ movie }) => {
   const { release_date, genres } = movie;
@@ -27,11 +30,26 @@ const Preview = ({ movie }) => {
           <div className="text-9xl font-bold">BLACK MIRROR</div>
           <div className="flex flex-grow justify-end items-center">
             <button
-              className={`font-extrabold rounded-3xl p-6 flex items-center h-5 ${styles.buttonBorder}`}
+              className={`font-semibold rounded-3xl p-6 flex items-center h-5 ${styles.buttonBorder}`}
             >
               PG 13
             </button>
           </div>
+        </div>
+        <div className="flex mt-12">
+          <div
+            style={{ backgroundColor: '#FF056C' }}
+            className="font-semibold rounded-xl px-3 py-1"
+          >
+            2.30h
+          </div>
+          <div className="flex ml-12 items-center">
+            <div>
+              <img src={clapperboard} alt="clapperboard" />
+            </div>
+            <div className="ml-4">Quentin Tarantino</div>
+          </div>
+          <StarRating filledStars={4} halfFilledStars={1} />
         </div>
       </div>
     </div>
