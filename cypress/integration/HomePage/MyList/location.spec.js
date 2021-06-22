a7a9d37053cce502de3aa8c32b2113c214e68ddf
Rafael Pirotto <pirotto@outlook.com>
2021-06-22 14:08:46 -0300

    Tests updated

--- cypress/integration/HomePage/MyList/location.spec.js
+++ cypress/integration/HomePage/MyList/location.spec.js
@@ -5,7 +5,7 @@ import { HOME_URL, TRAILERS_ROUTE } from '../../../constants';
 context('MyList', () => {
   beforeEach(() => {
     cy.viewport(1024, 768);
-    window.localStorage.setItem('token', process.env.REACT_APP_TOKEN);
+    localStorage.setItem('token', Cypress.env('token'));
     cy.visit(HOME_URL);
   });
 
--- cypress/integration/MyListPage/location.spec.js
+++ cypress/integration/MyListPage/location.spec.js
@@ -5,7 +5,8 @@ import { HOME_URL, MY_LIST_URL } from '../../constants';
 context('MyList', () => {
   beforeEach(() => {
     cy.viewport(1024, 768);
-    window.localStorage.setItem('token', process.env.REACT_APP_TOKEN);
+    localStorage.setItem('token', Cypress.env('token'));
+    'token', process.env.REACT_APP_TOKEN;
     cy.visit(MY_LIST_URL);
   });
 
