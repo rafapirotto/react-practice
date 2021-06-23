24b3a65263c0e33bcf9cff7304d0615aa9a195c5
Rafael Pirotto <pirotto@outlook.com>
2021-06-23 15:21:18 -0300

    Logout bug fixed

--- src/app/Components/Header/Components/HeaderOptionList/index.jsx
+++ src/app/Components/Header/Components/HeaderOptionList/index.jsx
@@ -7,13 +7,13 @@ import search from '../../assets/search.png';
 
 import styles from './styles/HeaderOptionList.module.css';
 import { USER_PROFILE_ROUTE, LOGIN_ROUTE } from '../../../../routes';
-import { SUCCESS } from '../../../../../common/constants';
 import { getUserProfile } from '../../../../Containers/UserProfileContainer/duck/operations';
 import { logout } from '../../../../utils';
 import logoutBtn from './assets/logout.png';
+import defaultUserImage from './assets/default_user_image.png';
 
 const HeaderOptionList = () => {
-  const { state, content } = useSelector((state) => state.userProfile);
+  const { content } = useSelector((state) => state.userProfile);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -22,24 +22,22 @@ const HeaderOptionList = () => {
   }, []);
 
   return (
-    state === SUCCESS && (
-      <div className="flex items-center flex-grow justify-end">
-        <HeaderOption src={search} alt="search" styles={styles.search} />
-        <HeaderOption
-          src={content.photo_path}
-          alt="user"
-          styles={styles.userImage}
-          to={USER_PROFILE_ROUTE}
-        />
-        <HeaderOption
-          src={logoutBtn}
-          alt="logout"
-          styles={styles.logoutBtn}
-          to={LOGIN_ROUTE}
-          onClick={logout}
-        />
-      </div>
-    )
+    <div className="flex items-center flex-grow justify-end">
+      <HeaderOption src={search} alt="search" styles={styles.search} />
+      <HeaderOption
+        src={content?.photo_path || defaultUserImage}
+        alt="user"
+        styles={styles.userImage}
+        to={USER_PROFILE_ROUTE}
+      />
+      <HeaderOption
+        src={logoutBtn}
+        alt="logout"
+        styles={styles.logoutBtn}
+        to={LOGIN_ROUTE}
+        onClick={logout}
+      />
+    </div>
   );
 };
 
